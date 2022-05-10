e4f582a2d5a63b31404abf4e42fe4933f3f3c227
Clinto_92_Abraham <clinto92@gmail.com>
2022-05-10 20:21:27 +0530

    Add = Stepper (Custom & Library :rsuite); Delete = MUI Header; Add = react-router-dom with sideBar;

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,55 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
-import Header from './Views/header';
+import RsuiteStepper from './Views/Stepper/Library'
+import StepIndex from './Views/Stepper/Custom';
+import SideBar from './Views/SideBar';
 
-function App() {
+ const routes = [
+  {
+    path: "/",
+    main: () => <img src={logo} className="App-logo" alt="logo" />
+  },
+  {
+    path: "/stepIndex",
+    main: () => <StepIndex />
+  },
+  {
+    path: "/rsuite-stepper",
+    main: () => <RsuiteStepper />
+  }
+];
+
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Header />
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Add new components inside <code>src/Components</code> and also add the each view page or components inside <code> src/Views</code>.
-        </p>
-        
-      </header>
-    </div>
+    <> 
+    <Router>
+      <div style={{ display: "flex" }}>
+        <SideBar />
+        <div style={{ flex: 1, padding: "10px" }}>
+          <Routes>
+            <> 
+            {routes.map((route, index) => (
+              <Route
+                key={index}
+                path={route.path}
+                element={<route.main />}
+              />
+            ))}
+            </>
+          </Routes>
+        </div>
+
+
+      </div>
+    </Router>
+    </>
   );
 }
 
-export default App;
